b548cc66221e7a2a538f19d15a5b421db23523e0
Hakan ÖZER <hakanozer02@gmail.com>
2022-08-11 10:15:21 +0300

    Security

--- RestFull/src/main/java/com/works/configs/SecurityConfig.java
+++ RestFull/src/main/java/com/works/configs/SecurityConfig.java
@@ -1,23 +1,43 @@
 package com.works.configs;
 
+import com.works.services.AdminDetailService;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    final PasswordEncoder encoder;
+    final AdminDetailService detailService;
+    public SecurityConfig(PasswordEncoder encoder, AdminDetailService detailService) {
+        this.encoder = encoder;
+        this.detailService = detailService;
+    }
+
     // sql query -> admin and role control
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+        auth.userDetailsService(detailService).passwordEncoder(encoder);
     }
 
     // role -> rest controller router
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().formLogin().disable(); // Security config disable
+
+        http
+        .httpBasic()
+        .and()
+        .authorizeRequests()
+        .antMatchers("/product/**").hasRole("product")
+        .antMatchers("/note/**").hasRole("note")
+        .antMatchers("/customer/**").hasRole("customer")
+        .and().csrf().disable().formLogin().disable();
+
+        //http.csrf().disable().formLogin().disable(); // Security config disable
         http.headers().frameOptions().disable(); // h2-console vb security disables
     }
 
--- RestFull/src/main/java/com/works/services/AdminDetailService.java
+++ RestFull/src/main/java/com/works/services/AdminDetailService.java
@@ -1,21 +1,25 @@
 package com.works.services;
 
 import com.works.entities.Admin;
+import com.works.entities.AdminRole;
 import com.works.repositories.AdminRepository;
 import com.works.utils.ERest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @Service
+@Transactional
 public class AdminDetailService implements UserDetailsService {
 
     final AdminRepository aRepo;
@@ -27,7 +31,30 @@ public class AdminDetailService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        Optional<Admin> optionalAdmin = aRepo.findByUsernameEqualsIgnoreCase(username);
+        if (optionalAdmin.isPresent() ) {
+            Admin admin = optionalAdmin.get();
+            return new User(
+                    admin.getUsername(),
+                    admin.getPassword(),
+                    admin.isEnabled(),
+                    admin.isAccountNonExpired(),
+                    admin.isCredentialsNonExpired(),
+                    admin.isAccountNonLocked(),
+                    fncRoles( admin.getAdminRoles() )
+                    );
+        }else {
+            // username fail
+            throw new UsernameNotFoundException("userName not found!");
+        }
+    }
+
+    private Collection<? extends GrantedAuthority> fncRoles(List<AdminRole> adminRoles) {
+        Collection cl = new ArrayList<>();
+        for( AdminRole role : adminRoles ) {
+            cl.add( new SimpleGrantedAuthority(role.getRole()) );
+        }
+        return cl;
     }
 
     public ResponseEntity register(Admin admin) {
